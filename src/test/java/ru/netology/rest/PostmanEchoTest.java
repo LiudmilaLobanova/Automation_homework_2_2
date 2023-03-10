2bb8771d278c3e03d1a0651be424266d3f2c8a02
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-09 23:57:32 -0800

    Test fail

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
@@ -16,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
         .then()
                 .statusCode(200)
-                .body("data", equalTo("some interesting"))
+                .body("data", equalTo("some data"))
 
         ;
     }
