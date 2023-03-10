36174b9e36ae32fabcb51dccae40ae0a12b77879
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-09 23:54:18 -0800

    Test PostmanEchoTest added

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldPOSTSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some interesting")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("some interesting"))
+
+        ;
+    }
+}
